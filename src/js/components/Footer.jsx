8a6f5f59b9b2b4d4a5b602c1cd71d9da39b4a460
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-25 16:37:50 -0400

    fix store; dynamic footer copyright

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => (
     <footer className="Footer">
-        <p className="copyright">&copy; 2017. Brenton Cozby. View more projects at <a href="https://brentoncozby.com">brentoncozby.com</a>.</p>
+        <p className="copyright">&copy; {(new Date).getFullYear()}. Brenton Cozby. View more projects at <a href="https://brentoncozby.com">brentoncozby.com</a>.</p>
     </footer>
 )
 
--- src/js/store.js
+++ src/js/store.js
@@ -4,14 +4,20 @@ import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware(
-    reduxPromise,
-    thunk,
-    compose(typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f)
-)(createStore)
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(reduxPromise, thunk),
+    // other store enhancers if any
+);
 
 export default function configStore(initialState) {
-    const store = createStoreWithMiddleware(rootReducer, initialState)
+    const store = createStore(rootReducer, initialState, enhancer)
 
     if (module.hot) {
         module.hot.accept('./reducers', () => {
