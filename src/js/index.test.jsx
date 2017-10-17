7370486b08bd8467e51886ca4f9010bac0d7649c
Brenton Cozby <brenton.cozby@gmail.com>
2017-10-17 06:12:36 -0400

    add testing

--- src/js/index.test.jsx
+++ src/js/index.test.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import configStore from '../store.js'
-import App from './app-component.jsx'
+import configStore from './store.js'
+import App from './components/app-component.jsx'
 
 describe('app-component', () => {
-    it('renders with router and store without crashing', () => {
+    it('renders App with router and store without crashing', () => {
         const div = document.createElement('div')
         const store = configStore()
         
